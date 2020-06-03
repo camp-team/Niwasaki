9b9814a6ac0a70b9ffebf16c8d74116f46d35ba1
Atsushi-Noguchi <gunogunoatushi@gmail.com>
2020-06-03 21:42:08 +0900

    feat(github.actions.yml): github.actions追加 #39

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAQPPfepg0ZNRZql4QrQNoAswQL9V0VLd8',
+    authDomain: 'niwasaki-94c38.firebaseapp.com',
+    databaseURL: 'https://niwasaki-94c38.firebaseio.com',
+    projectId: 'niwasaki-94c38',
+    storageBucket: 'niwasaki-94c38.appspot.com',
+    messagingSenderId: '455635888605',
+    appId: '1:455635888605:web:d03982a8bafcbf4f082e34',
+    measurementId: 'G-ZKTFK4FSWH',
+  },
 };
